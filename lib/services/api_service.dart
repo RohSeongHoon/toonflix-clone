1337f44b66e0c3016733216e6b88fbcba8a36309
RohSeongHoon <scubarsh@naver.com>
2023-08-17 17:19:30 +0900

    외부 api 에서 json을 받아와 객체로 파싱하고 리턴하는 메소드 생성

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
   final String today = 'today';
 
-  void getTodaysTonns() async {
+  Future<List<WebtoonModel>> getTodaysTonns() async {
+    List<WebtoonModel> webtoonModelInstance = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.fromJson(webtoon);
+        webtoonModelInstance.add(toon);
+        print(toon.title);
+      }
+      return webtoonModelInstance;
     }
     throw Error();
   }
